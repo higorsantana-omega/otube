c988a307ea9a37d350ada9edb22cb11c8fdd37c5
Higor Santana <70420430+higorsantana-omega@users.noreply.github.com>
2022-02-22 16:10:55 -0300

    update

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,19 @@
-import express from "express";
+import express, { Request, Response, Router } from "express";
+import { getPlaylistVideos } from "usetube";
 
 const app = express();
 
-app.listen(3000, () => {
+app.use(express.json());
+
+const route = Router();
+
+app.get("/", async (req: Request, res: Response) => {
+  const playlist = await getPlaylistVideos(
+    "PLLBRimSpFRj7Lnq-JdW2n8iNUPcEydkcl"
+  );
+  res.json({ playlist });
+});
+
+app.listen(3060, () => {
   console.log("Rodando");
 });
